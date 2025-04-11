de2b80ea09874874c85564ff7927cd6ed4f4f84e
Yogita024 <102277141+Yogita024@users.noreply.github.com>
2025-04-11 18:07:05 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pymongo import MongoClient
 
 def create_spark_session(app_name="SentimentApp"):
     """
@@ -9,3 +10,11 @@ def create_spark_session(app_name="SentimentApp"):
         .master("local[*]") \
         .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2") \
         .getOrCreate()
+
+def get_mongo_collection(uri="mongodb://localhost:27017", db_name="sentiment_db", collection_name="predictions"):
+    """
+    Connect to MongoDB and return the specified collection.
+    """
+    client = MongoClient(uri)
+    db = client[db_name]
+    return db[collection_name]
