4e4de1bc5d8671ad01d0cdc23e8ab91f4759a24f
Yogita024 <102277141+Yogita024@users.noreply.github.com>
2025-04-11 15:25:53 +0530

    Create tweet_streamer.py

---
+++ src/tweet_streamer.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import json
+import logging
+import time
+from kafka import KafkaProducer
+
+logging.basicConfig(level=logging.INFO)
+
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda x: json.dumps(x).encode('utf-8')
+)
+
+TOPIC = 'twitter'
+
+def handle_success(metadata):
+    logging.info(f"Delivered to {metadata.topic} [partition {metadata.partition}] at offset {metadata.offset}")
+
+def handle_failure(exception):
+    logging.error("Delivery failed", exc_info=exception)
+
+def stream_data_from_csv(path):
+    columns = ["Tweet ID", "Entity", "Sentiment", "TweetContent"]
+    df = pd.read_csv(path, names=columns)
+
+    for _, row in df.iterrows():
+        message = {
+            "id": row["Tweet ID"],
+            "entity": row["Entity"],
+            "text": row["TweetContent"].replace(',', ' '),
+            "label": row["Sentiment"]
+        }
+        producer.send(TOPIC, value=message).add_callback(handle_success).add_errback(handle_failure)
+        time.sleep(1)  # optional: simulate real-time
+
+if __name__ == "__main__":
+    stream_data_from_csv("../data/twitter_validation.csv")
